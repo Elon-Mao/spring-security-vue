486da32883f96776b96d5dbd34fd5eb229288cee
Elon-Mao <elonmao.engineer@gmail.com>
2023-11-15 16:17:03 +0800

    add README

---
+++ README.md
@@ -0,0 +1,3 @@
+# Spring Security + Vue
+
+The project uses JWT to authenticate user, adding JWT to cookie when login.
--- vue-module/vite.config.ts
+++ vue-module/vite.config.ts
@@ -14,19 +14,11 @@ export default defineConfig({
   server: {
     port: 7777,
     proxy: {
-      // 带选项写法：http://localhost:5173/api/bar -> http://jsonplaceholder.typicode.com/bar
       '/api': {
         target: 'http://localhost:8080',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-      // 正则表达式写法：http://localhost:5173/fallback/ -> http://jsonplaceholder.typicode.com/
-      // '^/api/.*': {
-      //   target: 'http://localhost:8080',
-      //   changeOrigin: true,
-      //   rewrite: (path) => path.replace(/^\/api/, ''),
-      // }
-    },
-    // cors: false
+      }
+    }
   }
 })
